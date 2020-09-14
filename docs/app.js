b23bf0c361756dea96be8eb8465b4cc5ee36ea77
poyu <weairch@hotmail.com>
2020-09-14 19:13:24 +0800

    week-2 assignment-4

---
+++ docs/app.js
@@ -0,0 +1,11 @@
+
+function change_css(){
+    document.getElementsByClassName('box2')[0].style.display="flex";
+    document.getElementsByClassName('box2')[1].style.display="flex";
+    document.getElementsByClassName('box2')[2].style.display="flex";
+    document.getElementsByClassName('box2')[3].style.display="flex";
+}
+
+function change_text(){
+    document.getElementById('change_text').innerHTML='Hava a Good Time!';
+}
