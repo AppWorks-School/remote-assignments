1b64f8ac1487c326e0dd97b5b4cbee389178d49f
poyu <weairch@hotmail.com>
2020-09-15 09:21:16 +0800

    finish max.js

--- week-2/Assignment-1/max.js
+++ week-2/Assignment-1/max.js
@@ -1,18 +1,17 @@
-// 1. max: find the max value of an array of numbers
-// 2. findPosition: find the first position of the target number inside an array of numbers. The
-// position should be counted starting from 0, if you can't find the target, please return -1
-// Reminder: you cannot use those built-in functions like Math.max() and
-// Array.prototype.findIndex() to complete this assignment, please implement it by yourself.
-// function max(numbers) {
-// // your code here, for-loop method preferred
-// }
-// function findPosition(numbers, target) {
-// // your code here, for-loop method preferred
-// }
 
-// console.log( max([1, 2, 4, 5]) ); // should print 5
-// console.log( max([5, 2, 7, 1, 6]) ); // should print 7
-// console.log( findPosition([5, 2, 7, 1, 6], 5) ); // should print 0
-// console.log( findPosition([5, 2, 7, 1, 6], 7) ); // should print 2
-// console.log( findPosition([5, 2, 7, 7, 7, 1, 6], 7) ); // should print 2 (the first position)
-// console.log( findPosition([5, 2, 7, 1, 6], 8) ); // should print -1 
+//取數列最大值 先創一個空數列 裡面擺放數列number的第一個值 如果比空數列大 就往裡面擺 
+var numbers = [ 1, 2,99, 4 ,5,10,]
+
+function max(numbers) {
+    var maxmax = numbers[0]                         //空數列
+    for (let i = 0 ; numbers.length > i ; i++){     //for 迴圈 跑每一個數字
+        if (numbers[i] > maxmax){                   //比對數字
+            maxmax = numbers[i]                     //比較大就放進數列
+        }
+    }
+    return maxmax                                   //回傳最大值
+}
+
+
+
+console.log(max(numbers))
