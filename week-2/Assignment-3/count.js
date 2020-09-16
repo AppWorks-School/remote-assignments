427c43eeb4428d7918028de09d354d4653ae6123
poyu <weairch@hotmail.com>
2020-09-16 14:41:39 +0800

    finish count.js

--- week-2/Assignment-3/count.js
+++ week-2/Assignment-3/count.js
@@ -1,27 +1,25 @@
-// 1. count: return an object which shows the count of each characters.
-// 2. groupByKey: return an object which shows the summed up value of each keys.
-// This time, you may get letters from ‘a’ to ‘z’, try to avoid using ‘if’ or ‘switch’ to split each letter
-// into different cases (e.g. if(letter == ‘a’) {…} else if (letter == ‘b’) {…} ), otherwise, your code
-// will be very long.
-// Note:
-// 1. The input format is different for these two functions.
-// 2. In the second function, the input may have same key but different values, the output
-// should have each key only once.
-// function count(input) {
-// // your code here
-// }
-// let input1 = ['a', 'b', 'c', 'a', 'c', ‘a’, ’x’];
-// console.log(count(input1));
-// // should print {'a':3, 'b':1, 'c':2, ‘x':1}
-// function groupByKey(input) {
-// // your code here
-// }
-// let input2 = [
-//  {key: 'a', value: 3},
-//  {key: 'b', value: 1},
-//  {key: 'c', value: 2},
-//  {key: 'a', value: 3},
-//  {key: 'c', value: 5}
-// ]
-// console.log(groupByKey(input2));
-// // should print {'a':6, 'b':1, ‘c’:7}
+
+//先創一個空物件 把所有字母都拿出來  當物件裡面沒有的話 就擺進字母:1(else)   如果有的話 就把後面數字++(if) 
+
+input=['a', 'b', 'c', 'a', 'c', 'a','x' ];
+
+
+function count(input){
+    var sum={}
+    for(let i=0 ;input.length > i ; i++){
+        if (input[i] in sum){
+            sum[input[i]]++
+        }
+        else {
+            var letter={};
+            letter[input[i]]=1;
+            sum = Object.assign(sum,letter)
+        }
+    }
+    return sum
+}
+
+
+console.log(count(input))
+//應該回傳 {'a':3, 'b':1, 'c':2, ‘x':1}
+
