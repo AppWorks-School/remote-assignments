ebf393630bb6762160f7e0b339ffde790f10a21e
poyu <weairch@hotmail.com>
2020-09-16 15:16:22 +0800

    finish count.js

--- week-2/Assignment-3/count.js
+++ week-2/Assignment-3/count.js
@@ -5,15 +5,16 @@ input=['a', 'b', 'c', 'a', 'c', 'a','x' ];
 
 
 function count(input){
-    var sum={}
-    for(let i=0 ;input.length > i ; i++){
-        if (input[i] in sum){
+    var sum={}                                  //空的物件
+    for(let i=0 ;input.length > i ; i++){       //讓陣列裡面的都先用迴圈拿出來
+        if (input[i] in sum){                   //如果input[i]有在sum裡面 就+1
             sum[input[i]]++
         }
-        else {
-            var letter={};
-            letter[input[i]]=1;
-            sum = Object.assign(sum,letter)
+        else {                                  //當迴圈每跑一次 sum{} 裡面沒有東西
+            var letter={};                      //就會把字母:1擺進去
+            letter[input[i]]=1;                 //每else 一次就會有一個字母:1 可是這樣會有4個物件{字母:1}
+            sum = Object.assign(sum,letter)     //Object.assign比較像複製 letter是來源 sum是擺放的目標  最終會把四個物件 變成在sum{}裡面
+            console.log(letter)
         }
     }
     return sum
