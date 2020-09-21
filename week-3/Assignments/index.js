15b749c2684b3859ab58d630dd5bb22e4ba3f222
poyu <weairch@hotmail.com>
2020-09-21 19:03:00 +0800

    finish assignment-2

--- week-3/Assignments/index.js
+++ week-3/Assignments/index.js
@@ -1,10 +1,41 @@
 var express = require('express');
-var index = express();
+var app = express();
 
 
-index.get('/',function(req,res) {
+//寫number以下的函數
+//如果number=5 回傳1+2+3+4+5
+//如果number='正整數' 回傳1+2+3...+正整數  
+
+
+app.get('/',function(req,res) {
     res.send('Hello , world');
 });
-index.listen(3000,function(){
+
+//卡關點 number無法用for計算?
+app.get('/getData',function(req,res){
+    var number = req.query.number
+    
+    if (number == null){                       
+        res.send("Lack of Parameter");
+    }
+
+    else if (number == '正整數'){
+        res.send('1+2+....+正整數')
+    }
+    else if (!isNaN(number) == true){           
+        var result=''
+        for( let i=1 ; number >= i ; i++){
+            result += i+'+'
+        }
+        last=result.substring(0,result.length-1)   //刪除最後一個字元
+        res.send(last)
+    }
+
+    else {
+        res.send("Wrong Parameter")
+    }
+}
+)
+app.listen(3000,function(){
     console.log('Server is working')
 })
