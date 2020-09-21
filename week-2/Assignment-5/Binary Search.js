923ac24f8431ff0d67cfee2316156ca84c87e36c
poyu <weairch@hotmail.com>
2020-09-21 10:24:42 +0800

    try to write Binary Search

---
+++ week-2/Assignment-5/Binary Search.js
@@ -0,0 +1,23 @@
+var array=[2,5,3,1,6]
+
+
+//在搜尋前 需要先排序數列 由小到大
+//先把所有數字拿出來 比大小 如果最小就擺到最左邊 越大擺越右邊
+//如何知道最小? 要在一個array裡面先找到最小值 才能夠排序
+
+function min(array){
+    for (let i=0; array.length>i ; i++){
+        
+    }
+}
+
+
+function sort(array){
+    number=[]
+    for (let i=0 ; array.length > i ;i++){
+        console.log(array[i])
+    }
+    return number
+}
+
+console.log(sort(array))
