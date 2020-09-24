5c4fd647822431816f1d7c94f87e1c0faffc3142
poyu <weairch@hotmail.com>
2020-09-24 15:19:37 +0800

    finish assignment 3

--- week-3/Assignments/index.js
+++ week-3/Assignments/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/',function(req,res) {
     res.send('Hello , world');
@@ -20,7 +23,7 @@ app.get('/getData',function(req,res){
         for( let i=1 ; number >= i ; i++){
             result += i+'+'
         }
-        last=result.substring(0,result.length-1)   //刪除最後一個字元
+        last=result.substring(0,result.length-1)   
         res.send(last)
     }
 
@@ -30,7 +33,20 @@ app.get('/getData',function(req,res){
 }
 )
 
-app.use(express.static(__dirname + '/assignment-3'));
+// assignment -3.4
+app.post('/sum',urlencodedParser,function(req,res){
+    console.log('namber:',req.body.num)
+    var number = req.body.num
+    var total=0
+    for (let i=0 ; number >=i ; i++){
+        total+=i
+    }
+    res.send('答案是'+total)
+})
+
+
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.listen(3000,function(){
